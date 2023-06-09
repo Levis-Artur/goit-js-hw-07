f22b4c55fc8785ee643a525ddf10c64a9efe7d53
Levis Artur <120251909+Levis-Artur@users.noreply.github.com>
2023-06-09 14:32:05 +0300

    3

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,13 +1,19 @@
-import { galleryItems } from "./gallery-items.js"; // Change code below this
-line const gallery = document.querySelector(".gallery"); function
-createGalleryItemsMarkup(items) { return items .map(({ preview, original,
-description }) => { return `
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line //
+const gallery = document.querySelector(".gallery");
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
 <li class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img class="gallery__image" src="${preview}" alt="Image description" />
   </a>
 </li>
-`; }) .join(""); } const galleryItemMarkup =
-createGalleryItemsMarkup(galleryItems); gallery.insertAdjacentHTML("beforeend",
-galleryItemMarkup); new SimpleLightbox(".gallery a", { captions: true,
-captionDelay: 250, });
+`;
+    })
+    .join("");
+}
+const galleryItemMarkup = createGalleryItemsMarkup(galleryItems);
+gallery.insertAdjacentHTML("beforeend", galleryItemMarkup);
+new SimpleLightbox(".gallery a", { captions: true, captionDelay: 250 });
